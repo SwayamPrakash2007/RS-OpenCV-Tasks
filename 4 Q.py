9b8ebba682530d388621afd347b9ffa3ee708149
SwayamPrakash2007 <swayamsbp@gmail.com>
2026-06-13 12:33:43 +0530

    Update 4 Q.py

--- 4 Q.py
+++ 4 Q.py
@@ -14,31 +14,26 @@ while True:
 
     h, w = frame.shape[:2]
 
-    # ---------------- Part 1 ----------------
-    # Half-size grayscale image
-    half = cv2.resize(frame, (w//2, h//2))
+    # Half-size grayscale display
+    half = cv2.resize(frame, (w // 2, h // 2))
     gray = cv2.cvtColor(half, cv2.COLOR_BGR2GRAY)
-
     cv2.imshow("Half Size Grayscale", gray)
 
-    # ---------------- Part 2 ----------------
-    # Quarter-size image
-    small = cv2.resize(frame, (w//2, h//2))
+    # Quarter-size image for 2x2 grid
+    small = cv2.resize(frame, (w // 2, h // 2))
 
     # Top-Left: Original
     tl = small.copy()
 
-    # Top-Right: Vertically flipped
+    # Top-Right: Vertically Flipped
     tr = cv2.flip(small, 0)
 
     # Bottom-Left: HSV
-    hsv = cv2.cvtColor(small, cv2.COLOR_BGR2HSV)
-    bl = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    bl = cv2.cvtColor(small, cv2.COLOR_BGR2HSV)
 
-    # Bottom-Right: Red channel only
-    red = np.zeros_like(small)
-    red[:, :, 2] = small[:, :, 2]
-    br = red
+    # Bottom-Right: Red Channel Only
+    br = np.zeros_like(small)
+    br[:, :, 2] = small[:, :, 2]
 
     # Create 2x2 grid
     top = cv2.hconcat([tl, tr])
